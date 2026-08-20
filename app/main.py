a19b0953cd2a3570f8cf161aeafe866f5561e772
Shihab Ahmed <shihab099999@gmail.com>
2026-08-20 22:23:24 +0600

    Create NirapodNet FastAPI server

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fast api import FastAPI
+from fastapi import FastAPI
 
 app = FastAPI(title="NirapodNet")
+
 
 @app.get("/")
 def root():
     return {
         "system": "NirapodNet",
-        "status": "online",
+        "status": "online"
     }
